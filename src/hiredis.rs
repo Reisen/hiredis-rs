46b15e5cc63d325206465d73def91870f3839b8c
Reisen <reisen@morphism.org>
2014-09-20 21:26:19 +0100

    Partially sort out lifetimes for Results

--- src/hiredis.rs
+++ src/hiredis.rs
@@ -13,6 +13,7 @@ pub mod api;
 
 
 pub struct Reply {
+    head:  bool,
     reply: *const api::Reply
 }
 
@@ -27,12 +28,27 @@ pub enum ReplyCode {
 }
 
 impl Reply {
-    unsafe fn new(reply: *const api::Reply) -> Reply {
+    /**
+     * Creates a new Reply wrapper.
+     *
+     * This wraps an already existing Reply returned from hiredis, it will
+     * automatically free the data when the reply goes out of scope. Note that
+     * this method cannot be invoked outside of the crate.
+     */
+    fn new(reply: *const api::Reply) -> Reply {
         Reply {
+            head:  true,
             reply: reply
         }
     }
 
+    /**
+     * Redis replies with the type of data being sent, this method allows
+     * peaking into it.
+     *
+     * TODO: Create a nicer wrapper enum that encodes the data directly with
+     * Vectors and so on.
+     */
     pub fn typename(&self) -> ReplyCode {
         unsafe {
             match (*self.reply)._type {
@@ -46,12 +62,40 @@ impl Reply {
             }
         }
     }
+
+    pub fn string<'r>(&'r self) -> Option<&'r [u8]> {
+        match self.typename() {
+            String => {
+                unsafe {
+                    let data = std::c_vec::CVec::new(
+                        (*self.reply)._str as *mut u8,
+                        (*self.reply).len as uint
+                    ).as_mut_slice() as *mut [u8];
+
+                    Some(&*data)
+                }
+            }
+
+            _ => {
+                None
+            }
+        }
+    }
 }
 
 impl Drop for Reply {
     fn drop(&mut self) {
         unsafe {
-            api::freeReplyObject(transmute(self.reply))
+            /* When Redis returns a Reply object, we wrap it in our own Reply
+             * object. However, if redis returns an array, the Reply object
+             * contains an array of sub Reply objects. These get wrapped too,
+             * but should not be freed, as freeing the head object frees the
+             * children too. This check prevents us accidentally freeing replies
+             * that shouldn't be freed.
+             */
+            if self.head {
+                api::freeReplyObject(transmute(self.reply))
+            }
         }
     }
 }
@@ -61,6 +105,12 @@ pub struct Redis {
 }
 
 impl Redis {
+    /**
+     * Creates a new connection to a Redis instance.
+     *
+     * TODO: Deal with the other redisConnect* variants, not yet sure how to
+     * design this API.
+     */
     pub fn new(ip: &str, port: i32) -> Redis {
         unsafe {
             Redis {
@@ -72,10 +122,18 @@ impl Redis {
         }
     }
 
-    /* TODO: Move to IoResult using context.err */
-    pub fn exec(&self, command: &str) -> Option<Reply> {
+    /**
+     * Sends a single command to be executed, and returns the reply immediately.
+     * If further replies are expected the `receive` function exists to expect
+     * them. This function doesn't handle complex spaces in messages, and so the
+     * `execf` function exists and uses hiredis's formatting instead.
+     *
+     * TODO: Move to IoResult using context.err
+     */
+    pub fn command(&self, command: &str) -> Option<Reply> {
         command.with_c_str(|v| {
             unsafe {
+                /* This is the library call, returns (void*)0 on failure. */
                 let result = api::redisCommand(self.context, v);
 
                 /* Fail if the command errored for some reason. */
@@ -83,13 +141,30 @@ impl Redis {
                     return None;
                 }
 
-                /* Otherwise transmute the void pointer memory into a pointer
-                 * to a reply structure and return it. */
-                Some(Reply::new(transmute(result)))
+                /* Otherwise cast the pointer to the actual struct type being
+                 * returned and store it to be returned. */
+                Some(Reply::new(result as *const api::Reply))
             }
         })
     }
 
+    /**
+     * Appends a command to the output buffer, allowing for pipelining.
+     *
+     * TODO: This needs to return a status code.
+     */
+    pub fn append_command(&self, command: &str) {
+        command.with_c_str(|v| {
+            unsafe {
+                api::redisAppendCommand(self.context, v);
+            }
+        });
+    }
+
+    /**
+     * Attempts to read a message from the input buffer. Blocking in this
+     * context depends on how the connection was created.
+     */
     pub fn receive(&self) -> Option<Reply> {
         unsafe {
             let reply: Reply = std::mem::uninitialized();
@@ -101,6 +176,11 @@ impl Redis {
 }
 
 impl Drop for Redis {
+    /**
+     * When a Redis instance goes out of scope, the connection needs to be
+     * closed. Redis has a function for this that both closes the connection and
+     * frees any resources used.
+     */
     fn drop(&mut self) {
         unsafe {
             api::redisFree(self.context)
